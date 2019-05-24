954dd9fcee6f9dca6c3a11ccc73b0d47de650890
Sanjeev Mishra <sanjeevroy56@gmail.com>
2019-05-24 22:16:42 +0530

    Works  for all test cases

--- Neighbours and NewYear Party/Neighbours and NewYear Party.cpp
+++ Neighbours and NewYear Party/Neighbours and NewYear Party.cpp
@@ -1,38 +1,64 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <queue> 
+#include <bits/stdc++.h> 
+
 using namespace std;
+void reverseQueue(queue<int>& Queue) 
+{ 
+    stack<int> Stack; 
+    while (!Queue.empty()) { 
+        Stack.push(Queue.front()); 
+        Queue.pop(); 
+    } 
+    while (!Stack.empty()) { 
+        Queue.push(Stack.top()); 
+        cout<<Stack.top();
+        Stack.pop(); 
+    } 
+}
+
 int main()
 {
-	int t,n,i,j,k,bax,y;
-	int* a;
-	cin>>t;
-	for(i=0;i<t;i++)
-  	{
-		cin>>n;
-		int flag=0;
-		int z=0;
-		a = (int*)calloc(n,sizeof(int));
-		for(j=0;j<n;j++)
-    		{
-			cin>>a[j];
-		}
-		int *max=std::max_element(a, a + n);
-		bax = *max;
-		for(j=0;j<n-2;j++)
-    		{
-			for(k=j+2;k<n;k++)
- 			{
-			    if((a[j]+a[k]>=bax)&&(a[k]>=z)&&(a[k]!=0)&&(a[j]!=0))
-			    {
-				bax=a[j]+a[k];
-				z=a[k];y=a[j];
-				flag=1;
-			    }
-			}
-		}
-		if(flag==1)
-		  cout<<z<<y<<endl;
-		else
-		  cout<<bax<<endl;
-	}	
+    int i,j,n,t;
+    cin>>t;
+    for(j=0;j<t;j++){
+    cin>>n;
+    int a[n];
+    queue <int> inc;
+    queue <int> exc;
+    queue <int> exc1;
+    for(i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int incl=a[0],excl=0,count=0,max;
+    inc.push(a[0]);
+    for(i=1;i<n;i++){
+    int nw = (incl>excl)? incl:excl;
+    if(nw==incl){
+      exc1 = inc;
+      count++;
+    }
+    incl = excl + a[i];
+    inc=exc;
+    if(a[i]==0)
+    goto skip;
+    inc.push(a[i]);
+    skip: excl = nw;
+    if(count==1){
+        exc=exc1;
+        count=0;
+    }
+    }
+    if(incl==excl){
+        max =  (inc.back()>exc.back())? inc.back():exc.back();
+    }
+    else{
+    max = (incl>excl)? incl:excl;}
+    if((max==incl)||(max==inc.back())){
+        reverseQueue(inc);
+    }
+    else if((max==excl)||(max==exc.back()))
+    reverseQueue(exc);
+    }
+    
 }
